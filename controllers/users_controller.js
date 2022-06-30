f09432dc53229bbdf5e4ecbd02a268576d8090be
nandini <nandini.shrivastav_cs20@gla.ac.in>
2022-06-30 17:00:49 +0530

    Adding Sign in manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,28 @@
 const User = require('../models/users')
 module.exports.profile = function (req, res) {
-    return res.render('home', {
-        title: "Profile page"
-    })
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function (err, user) {
+            if (err) {
+                console.log('error in finding cookie details');
+            }
+            if (req.cookies.user_id) {
+                return res.render('profile', {
+                  title:"profile page",
+                  user:user
+
+
+                }
+                )
+            }
+            else {
+                return res.redirect('/users/sign-in');
+            }
+
+        });
+    }
+    else {
+        return res.redirect('/users/sign-in');
+    }
 }
 module.exports.signup = function (req, res) {
     return res.render('signup', {
@@ -20,32 +40,71 @@ module.exports.create = function (req, res) {
         return res.redirect('back');
     }
     User.findOne({
-        email: req.body.email}, function(err, user) {
-            if (err) {
-                console.log('error in finding the email');
-                return
+        email: req.body.email
+    }, function (err, user) {
+        if (err) {
+            console.log('error in finding the email');
+            return
+        }
+        if (!user) {
+
+            User.create(req.body, function (err, user) {
+                console.log(req.body);
+                if (err) {
+                    console.log('error in creating user for sign- up');
+                    return
+
+                }
+                return res.redirect('/users/sign-in')
+            })
+        }
+        else {
+            return res.redirect('back');
+        }
+
+    }
+    );
+
+}
+//sign in and create a session for the user
+module.exports.createsession = function (req, res) {
+    //steps to authenticate
+
+    //find the user
+    User.find({ email: req.body.email }, function (err, user) {
+        if (err) {
+            console.log('error in finding user in signing in');
+            return;
+        }
+        //handle user found
+        console.log(user);
+        if (user) {
+            //handle password which don't match
+            if (user[0].password == req.body.password) {
+                console.log("signed in details of user... ")
+                console.log(req.body.password);
+                console.log(req.body.email);
+
             }
-            if (!user) {
-              
-                User.create(req.body, function (err, user) {
-                    console.log(req.body);
-                    if (err) {
-                        console.log('error in creating user for sign- up');
-                        return 
-                       
-                    }
-                    return res.redirect('/users/sign-in')
-                })
-            }
-            else{
+            else {
+                console.log("check your password..")
+                console.log("password" + " " + user[0].password + "and  " + req.body.password + "does not match")
                 return res.redirect('back');
             }
 
+            //handle session-creation
+            res.cookie('user_id', user[0].id);
+            return res.redirect('/users/profile');
         }
-    );
+        else {
+            //handle user not found
+            return res.redirect('back');
+        }
+    })
 
- }
-//sign in and create a session for the user
-module.exports.createsession = function (req, res) {
+
+
+
+
 
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,5 @@ router.get('/profile', userController.profile);
 router.get('/sign-up', userController.signup);
 router.get('/sign-in', userController.signin);
 router.post('/create', userController.create);
+router.post('/create-session', userController.createsession);
 module.exports = router;
